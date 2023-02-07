cd047fb763eeaea71c58dcf4cc1ed3edc9dae8c8
NishithHM <nishithhm@Apples-MacBook-Pro-2.local>
2023-02-07 06:58:38 +0530

    enabling cors for dev

--- index.js
+++ index.js
@@ -4,12 +4,15 @@ const bodyParser = require('body-parser');
 const dotenv = require('dotenv')
 const dbCon = require('./db/connection')
 const cookieParser = require("cookie-parser");
-
+const cors = require('cors')
 
 const app = express()
 dotenv.config({ path: './.env' })
 dbCon.connect()
 const port = process.env.PORT;
+if(process.env.ENV==='dev'){
+    app.use(cors())
+}
 app.use(cookieParser())
 app.use(bodyParser.json({ limit: '1mb' }))
 app.use(router)
