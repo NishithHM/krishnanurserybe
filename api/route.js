dd7d3e94bff36da0ce3aebf3e2f862dada3bed18
NishithHM <nishithhm@Apples-MacBook-Pro-2.local>
2023-03-23 13:34:38 +0530

    added images

--- api/route.js
+++ api/route.js
@@ -50,7 +50,7 @@ router.get('/api/category/getAll', [authWall(['admin', 'procurement']), paramsTo
 router.put('/api/category/delete/:id', [authWall(['admin'])], paramsToBody(['id'], 'params'), bodyValidator(deleteCategorySchema), deleteCategoryById)
 
 // procurements
-router.post('/api/procurements/create', [authWall(['procurement']), uploadInvoice.array('invoice', 3), paramsToBody(['body'], 'formData'), bodyValidator(createProcurementSchema)], addNewProcurement)
+router.post('/api/procurements/create', [authWall(['procurement']), uploadInvoice.array('invoice', 4), paramsToBody(['body'], 'formData'), bodyValidator(createProcurementSchema)], addNewProcurement)
 router.post('/api/procurements/update/:id', [authWall(['procurement']),  uploadInvoice.single('invoice'), paramsToBody(['body'], 'formData'), paramsToBody(['id'], 'params'), bodyValidator(updateProcurementSchema)], updateProcurement)
 router.get('/api/procurements/getAll', [authWall(['admin', 'procurement', 'sales', 'preSales']), paramsToBody(['pageNumber', 'search', 'isCount', 'sortBy', 'sortType'], 'query'), bodyValidator(getProcurementsSchema)], getAllProcurements)
 router.get('/api/procurements/getAllHistory', [authWall(['admin', 'procurement']), paramsToBody(['pageNumber', 'isCount', 'id', 'startDate', 'endDate', 'isAverage'], 'query'), bodyValidator(getProcurementsHistorySchema)], getAllProcurementsHistory)
