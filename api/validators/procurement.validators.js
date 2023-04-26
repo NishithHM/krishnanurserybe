eaae6974322d4eba6652cc19e50d800664427e0b
NishithHM <nishithhm@Apples-MacBook-Pro-2.local>
2023-04-26 18:34:39 +0530

    fixed

--- api/validators/procurement.validators.js
+++ api/validators/procurement.validators.js
@@ -52,7 +52,7 @@ exports.placeOrderSchema = Joi.object().keys({
     })),
     expectedDeliveryDate: Joi.string().pattern(new RegExp(/\d{4}-\d{2}-\d{2}/)),
     id: Joi.string(),
-    procurementId: Joi.string().required(),
+    procurementId: Joi.string(),
     currentPaidAmount: Joi.number().required(),
 
 });
