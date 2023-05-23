5bb76f7bf3dd39833c1208502575fb561bcacc44
Charith RC <Charith.RC@novigo.com>
2023-05-23 12:47:55 +0530

    DB url has been added to env

--- db/connection.js
+++ db/connection.js
@@ -4,7 +4,8 @@ const mongoose = require('mongoose')
 
 exports.connect = () => {
     const env = process.env.ENV;
-    mongoose.connect(`mongodb+srv://admin:admin123@cluster0.t2cxv.mongodb.net/nursery_mgmt_${env}?retryWrites=true&w=majority`, {
+    const db = process.env.DB;
+    mongoose.connect(`${db}/nursery_mgmt_${env}?retryWrites=true&w=majority`, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
     }).then(() => console.log("Database connected! "))
