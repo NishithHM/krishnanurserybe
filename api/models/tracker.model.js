03ecbea389c6cf64a50fee7339b2f9f0e1ddbc9c
NishithHM <nhm987@gmail.com>
2023-06-08 12:52:26 +0530

    fixed

--- api/models/tracker.model.js
+++ api/models/tracker.model.js
@@ -1,3 +1,4 @@
+const { isEmpty } = require('lodash')
 const mongoose = require('mongoose')
 const tracker = new mongoose.Schema({
 	name: {
@@ -14,11 +15,20 @@ const tracker = new mongoose.Schema({
 })
 
 const Tracker = mongoose.model("tracker", tracker)
-try {
-const initial = new Tracker({name:'invoiceId', number:00001})
-initial.save()
-     
-} catch (error) {
-     console.log(error)
+
+
+const createDefaults =async ()=>{
+     const data = await Tracker.findOne({name:'invoiceId'})
+     if(isEmpty(data)){
+     try {
+          const initial = new Tracker({name:'invoiceId', number:00001})
+          initial.save()
+               
+          } catch (error) {
+               console.log(error)
+          }
+     }
 }
+
+createDefaults()
 module.exports = Tracker;
