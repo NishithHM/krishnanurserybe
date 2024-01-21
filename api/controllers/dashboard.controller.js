b65d53705cb4e7d4b8fb789b2a04e36199aed9cc
NishithHM <nhm987@gmail.com>
2024-01-21 14:55:47 +0530

    added variants

--- api/controllers/dashboard.controller.js
+++ api/controllers/dashboard.controller.js
@@ -15,11 +15,11 @@ exports.dahboardMetaData = async (req, res) => {
        otherProcMatch._id = {$in:plantIds}
     }else if(categories?.length){
         categoryIds = categories.map(ele=> mongoose.mongo.ObjectId(ele))
-        otherMetaMatch["categories._id"]= {$in:categories}
-        otherProcMatch["categories._id"]= {$in:categories}
+        otherMetaMatch["categories._id"]= {$in:categoryIds}
+        otherProcMatch["categories._id"]= {$in:categoryIds}
     }
     
-    const pipelinePlants = [
+    const pipelineMeta = [
         {
             $match: {
                 date: { $gte: sDate, $lt: eDate },
@@ -30,9 +30,6 @@ exports.dahboardMetaData = async (req, res) => {
         {
             $group: {
                 _id: null,
-                profit: {
-                    $sum: "$profit",
-                },
                 sales: {
                     $sum: "$sales.totalSales",
                 },
@@ -44,8 +41,134 @@ exports.dahboardMetaData = async (req, res) => {
                 },
             },
         },
+        {
+            $addFields: {
+              profit: {
+                $subtract: ["$sales", "$investment"],
+              },
+            }
+        }
     ]
-    console.log(JSON.stringify(pipelinePlants))
+    const pipelinePlants = [
+        {
+            $match: {
+                date: { $gte: sDate, $lt: eDate },
+                type:"NURSERY",
+                ...otherMetaMatch
+            },
+            
+        },
+        {
+            $group: {
+              _id: "$procurementId",
+              sales: {
+                $sum: "$sales.totalSales",
+              },
+              investment: {
+                $sum: "$procurements.totalPrice",
+              },
+              damages: {
+                $sum: "$damages",
+              },
+            },
+          },
+          {
+            $addFields: {
+              profit: {
+                $subtract: ["$sales", "$investment"],
+              },
+              profitPercentage: {
+                $multiply: [
+                  100,
+                  {
+                    $divide: [
+                      {
+                        $subtract: [
+                          "$sales",
+                          "$investment",
+                        ],
+                      },
+                      {
+                        $cond: {
+                          if: { $eq: ["$investment", 0] },
+                          then: 1,
+                          else: "$investment",
+                        }
+                    }
+                    ],
+                  },
+                ],
+              },
+            },
+          },
+          {
+            $sort:
+              /**
+               * Provide any number of field/order pairs.
+               */
+              {
+                sales: -1,
+              },
+          },
+          {
+            $limit:
+              /**
+               * Provide the number of documents to limit.
+               */
+              10,
+          },
+        
+    ]
+
+    const pipelineVairants = [
+        {
+          $match: {
+            date: { $gte: sDate, $lt: eDate },
+                type:"NURSERY",
+                ...otherMetaMatch
+          },
+        },
+        {
+          $unwind:
+            /**
+             * path: Path to the array field.
+             * includeArrayIndex: Optional name for index.
+             * preserveNullAndEmptyArrays: Optional
+             *   toggle to unwind null and empty values.
+             */
+            {
+              path: "$bill_data",
+            },
+        },
+        {
+          $sort:
+            /**
+             * Provide any number of field/order pairs.
+             */
+            {
+              "bill_data.quantity": -1,
+            },
+        },
+        {
+          $group: {
+            _id: "$bill_data._id",
+            quantity: {
+              $sum: "$bill_data.quantity",
+            },
+            saleAmount: {
+              $sum: "$bill_data.saleAmount",
+            },
+          },
+        },
+        {
+          $limit:
+            /**
+             * Provide the number of documents to limit.
+             */
+            10,
+        },
+      ]
+    console.log(JSON.stringify(pipelineMeta))
     const pipelinePayments = [
         {
             $match: {
@@ -87,11 +210,17 @@ exports.dahboardMetaData = async (req, res) => {
         },
       ]
     console.log(JSON.stringify(pipelinePayments))
-    const metaData = await metaDataModel.aggregate(pipelinePlants)
+    const metaData = await metaDataModel.aggregate(pipelineMeta)
+    const plantsData = await metaDataModel.aggregate(pipelinePlants)
     const metaPayments = await metaDataModel.aggregate(pipelinePayments)
+    let variants =[]
+    if(plantIds?.length===1){
+        variants = await metaDataModel.aggregate(pipelineVairants)
+
+    }
     const quantity = await procurmentModel.aggregate(quantityPipeline)
     console.log(metaData)
-    const resp = {...metaData[0], ...metaPayments[0], ...quantity[0]}
+    const resp = {...metaData[0], ...metaPayments[0], ...quantity[0], plants: plantsData, variants}
     res.json(resp)
 }
 
@@ -106,7 +235,7 @@ exports.dahboardMetaGraph = async (req, res) => {
        otherMetaMatch.procurementId= {$in:plantIds}
     }else if(categories?.length){
         categoryIds = categories.map(ele=> mongoose.mongo.ObjectId(ele))
-        otherMetaMatch["categories._id"]= {$in:categories}
+        otherMetaMatch["categories._id"]= {$in:categoryIds}
     }
     
     const pipeline = [
@@ -173,7 +302,11 @@ exports.dahboardMetaGraph = async (req, res) => {
                   },
                 ],
               },
+              profit: {
+                $subtract: ["$totalSales", "$investment"],
+              },
             },
+            
         },
       ]
     
