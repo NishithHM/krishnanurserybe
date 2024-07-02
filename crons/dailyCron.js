8a956b3aa5a7419826dcc53052842bec3e8a995e
NishithHM <nhm987@gmail.com>
2024-07-02 18:03:23 +0530

    added code changes for payments

--- crons/dailyCron.js
+++ crons/dailyCron.js
@@ -293,6 +293,8 @@ exports.caluclateMetaData = async (currentDate) => {
           $gte: prevDate,
           $lt: currentDate,
         },
+        type:{$ne:"CAPITAL"},
+        businessType: "NURSERY"
       }
     },
     {
@@ -331,7 +333,7 @@ exports.caluclateMetaData = async (currentDate) => {
   const paymentData = await paymentModel.aggregate(paymentPipeline)
   if(paymentData[0]?.amount){
     delete paymentData._id
-    const metaData = new MetaData({ ...paymentData[0], type:'PAYMENT', date:currentDate})
+    const metaData = new MetaData({ ...paymentData[0], type:'PAYMENT', businessType: "NURSERY", date:currentDate})
     await metaData.save()
     // console.log(JSON.stringify(metaData))
   }
