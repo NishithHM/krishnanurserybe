4e19066c8d0822c2fb0868f9f0ad89edecdb827a
NishithHM <nhm987@gmail.com>
2024-07-27 10:41:46 +0530

    fixing customer billing phone number not mandtory

--- api/controllers/customer.controller.js
+++ api/controllers/customer.controller.js
@@ -77,6 +77,9 @@ exports.getCustomerByNumber = async (req, res)=>{
                           {
                             $eq: ["$customerId", "$$id"],
                           },
+                          {
+                            $eq: ["$status", "BILLED"],
+                          },
                         ],
                       },
                     },
