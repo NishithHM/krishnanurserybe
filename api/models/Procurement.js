df4f8aaedd63639efd6955210ac7053464212bb8
itzDivyanka <divyankapatil6194@gmail.com>
2024-09-25 05:29:26 +0530

    Quick commit for modified files

--- api/models/Procurement.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const { Schema } = mongoose;
-
-const ProcurementSchema = new Schema({
-    nameForCustomer: { type: String, required: true }, 
-    sellingPrice: { type: Number, required: true },    
-});
-
-module.exports = mongoose.model('Procurement', ProcurementSchema);
