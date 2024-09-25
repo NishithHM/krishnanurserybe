d7da4724e91757e332253721d1e6542107741da9
itzDivyanka <divyankapatil6194@gmail.com>
2024-09-25 05:38:14 +0530

    Quick commit for modified files

--- api/models/Cart.js
+++ api/models/Cart.js
@@ -1,3 +1,5 @@
+
+module.exports = mongoose.model('Cart', CartSchema);
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
@@ -10,15 +12,19 @@ const CartSchema = new Schema({
     },
     items: [
         {
-            procurementId: { type: mongoose.Schema.Types.ObjectId, required: true },  // Refers to procurement data
+            plantId: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'PlantInfo' },  // Plant info reference
             name: { type: String, required: true },  // Plant name
-            price: { type: Number, required: true }, // Price of the plant
-            qty: { type: Number, required: true }    // Quantity of the plant
+            price: { type: Number, required: true }, // Selling price of the plant
+            discountedPrice: { type: Number, required: true }, // Discounted price if available
+            qty: { type: Number, required: true },  // Quantity of the plant
+            tips: [String],  // Array of tips for plant care
+            moreInfo: { type: String },  // Additional plant information
+            tags: [String],  // Array of tag names
         }
     ],
     totalDiscount: { type: Number, default: 0 },   // Total discount applied
     totalAmount: { type: Number, required: true }, // Final total amount after discount
     couponCode: { type: String, default: null }    // Optional coupon code
-});
+}, { timestamps: true });
 
 module.exports = mongoose.model('Cart', CartSchema);
